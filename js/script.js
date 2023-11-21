fd406a941dc210ea43b6fa891ff0e233f9473051
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-11-22 00:13:36 +0100

    Added function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
 const button = document.querySelector('button');
 console.log(button)
 
-
-
 //click event//
 button.addEventListener("click", function() {
     
     const mainContentEl=document.querySelector('main section.container');
     console.log(mainContentEl)
 
-    for(let i = 0; i <= 99; i++){
+    for(let i = 1; i <= 100; i++){
         mainContentEl.appendChild(createSquare());
-    }   
+    }
+
+    let newSquare=document.querySelector('div.square');
+    console.log(newSquare)
+
+    //BG CLICK//
+    newSquare.addEventListener("click", function() {
+        newSquare.classList.add('clicked-blue')
+    })
+
+    //NUMBERS//
+    for(let numberSquare = 1; numberSquare <= 100; numberSquare++){
+       console.log(numberSquare)
+    }
+
 }); 
+
+//
 
 //FUNCTION//
 function createSquare(){
@@ -20,3 +34,5 @@ function createSquare(){
     newSquareEl.classList.add('square');
     return newSquareEl
 }
+
+
